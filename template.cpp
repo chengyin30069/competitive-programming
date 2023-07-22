c6d0d5d516cc0366f8caac753af486a6f7b25b8b
H1de_on_bruH <83698305+chengyin30069@users.noreply.github.com>
2023-07-22 21:01:46 +0800

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,78 @@
+/*
+
+Paul_Liao,PikaQSoWeak,EasonCookie197,pixelcat,balbit,wty2016,nathanlee726 are my idols
+
+https://imgur.com/rzV8Lqv
+My waifu~~<3
+
+我超電
+     ---Paul_Liao
+(sorce:https://imgur.com/4O6u9ke)
+
+
+*/
+#include<bits/stdc++.h>
+#include<bits/extc++.h>
+#define int long long
+#define float double
+#define F first
+#define S second
+#define pb push_back
+#define pii pair<int,int>
+#define mp make_pair
+#define endl "\n"
+#define FOR(i,x,y) for(int i=x;i<y;i++)
+#define REP(x) for(int i=0;i<x;i++)
+#define INF 1e16
+#define lowbit(x) x&-x
+#define MOD Mod1
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)(x.size())
+#define Yuubari_chan_kawaii ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+using namespace __gnu_pbds;
+template<class T>using deset=tree<T,null_type,greater<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<class T>using inset=tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<class T,class TT>using fm=gp_hash_table<T,TT>;
+template<class T>void output(const T&hs){for(auto i:hs)cout<<i<<" ";cout<<endl;}
+template<class T>void outpair(const T&hs){for(auto i:hs)cout<<i.F<<" "<<i.S<<endl;cout<<endl;}
+int gcd(int x,int y){return y?gcd(y,x%y):x;}
+const int Mod1=1000000007,Mod2=998244353,N=2e5+10;
+int fpow(int x,int y,int mod){
+	if(y==0)return 1;
+	if(y==1)return x;
+	int z=fpow(x,y/2,mod);
+	int w=(z*z)%mod;
+	if(y%2==1)return (w*x)%mod;
+	return w;
+}
+mt19937 ran(random_device{}());
+#define local
+void file()
+{
+#ifdef local
+	freopen("in.txt","r",stdin);
+#endif
+}
+void solve()
+{
+	int n;cin>>n;
+	while(n--)cout<<"test\n";
+}
+signed main()
+{
+	Yuubari_chan_kawaii
+	file();
+	solve();
+}
+/*
+
+__     __          _                _        _                   _                       _ _
+\ \   / /         | |              (_)      | |                 | |                     (_|_)
+ \ \_/ /   _ _   _| |__   __ _ _ __ _    ___| |__   __ _ _ __   | | ____ ___      ____ _ _ _
+  \   / | | | | | | '_ \ / _` | '__| |  / __| '_ \ / _` | '_ \  | |/ / _` \ \ /\ / / _` | | |
+   | || |_| | |_| | |_) | (_| | |  | | | (__| | | | (_| | | | | |   < (_| |\ V  V / (_| | | |
+   |_| \__,_|\__,_|_.__/ \__,_|_|  |_|  \___|_| |_|\__,_|_| |_| |_|\_\__,_| \_/\_/ \__,_|_|_|
+
+
+*/
