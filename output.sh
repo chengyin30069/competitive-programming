5bc4f541b66a5ed6efb4ebd330d6c22ec7c7363b
H1de_on_bruH <83698305+chengyin30069@users.noreply.github.com>
2023-10-18 15:51:32 +0800

    Create output.sh

---
+++ output.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+clear
+echo "Input file:"
+echo
+cat $1/in.txt
+echo
+echo "=========================="
+echo
+declare startTime=`date +%s%N`
+$1/out < $1/in.txt > $1/out.txt
+declare endTime=`date +%s%N`
+delta=`expr $endTime - $startTime`
+delta=`expr $delta / 1000000`
+cat $1/out.txt
+echo
+echo "time: $delta ms"
